ecb4b26aa5aaff07420bea8cb86194defc83a7bc
Guilherme Gare <ggare.contato@gmail.com>
2025-01-23 18:52:41 -0300

    verifyJWT adicionado a todas as rotas

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.post("/api/loginProfile", ProfileController.loginProfile);
+
+app.use(verifyJWT);
+
 app.post("/api/createProfile", ProfileController.createProfile);
-app.get("/api/getAllProfile", verifyJWT, ProfileController.getAllProfiles);
+app.get("/api/getAllProfile", ProfileController.getAllProfiles);
 app.get("/api/getProfile/:id", ProfileController.getProfile);
 app.delete("/api/deleteProfile/:id", ProfileController.deleteProfile);
 app.post("/api/editProfile", ProfileController.editProfile);
 app.post("/api/changePassword", ProfileController.changePassword);
-app.post("/api/loginProfile", ProfileController.loginProfile);
 
 app.listen(PORT, () => {
   console.log("Server running!");
